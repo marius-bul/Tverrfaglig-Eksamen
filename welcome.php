279dc28e22da36b842f5256b99549c3d8c399262
marius-bul <mariusbuljo@gmail.com>
2025-05-28 09:28:01 +0200

    fikset små feil og lagt til en table

--- welcome.php
+++ welcome.php
@@ -11,19 +11,18 @@ if (!isset($_SESSION['bruker_id'])) {
 <!DOCTYPE html>
 <html lang="no">
 <head>
-    <link rel="stylesheet" href="nav.css">
-    <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
     <title>Velkommen</title>
 </head>
 
 <body>
-  
 <!-- navigasjonsbaren min !-->
 <nav>
-    <div class="nav">
-   <!--<a href="#">Hjem</a>!-->    
-      <a href="supreme.php" tabindex="1">Rate Supreme</a>
+    <div class="navnav">
+    <!--<a href="#">Hjem</a>!-->
+
+      <a href="supreme.php" tabindex="1">Rate Gucci</a>
     </div>
 </nav>
 
@@ -31,7 +30,15 @@ if (!isset($_SESSION['bruker_id'])) {
   <!-- Det brukeren ser på siden!-->
   <h1>Velkommen, <?php echo $_SESSION['brukernavn']; ?>!</h1>
 
-  <p>Du er nå logget inn på siden, når du føler deg klar trykk på en av merkene oppe til høyre og begynn å rate!</p>
+  <p>Du er nå logget inn på siden, når du føler deg klar trykk på en av merkene oppe til høyre og begynne å rate!</p>
+
+<!--Bilder på siden for å fylle tom plass!-->
+
+<table>
+    <tr>
+         <td><img src="" height="" length="" alt=""></td>   <td>  </td>  
+    </tr>
+</table>
 
 </body>
 
